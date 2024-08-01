12d97302e884e5ce0b4d7c659246296d84109284
Eman Zeb <emanzeb0346@gmail.com>
2024-08-01 16:38:03 +0500

    publish branch

---
+++ Day 5/Day 14/Day 6/prgm8.py
@@ -0,0 +1,11 @@
+def multiply_numbers():
+
+  num1 = float(input("Enter the first number: "))
+  num2 = float(input("Enter the second number: "))
+
+  product = num1 * num2
+
+  print("The product is:", product)
+
+if _name_ == "_main_":
+  multiply_numbers()
