133152b7aa476375cbc73e8a43eb1806ecc1d2d8
Eman Zeb <emanzeb0346@gmail.com>
2024-08-01 17:28:11 +0500

    publish branch

--- Day 5/Day 14/Day 6/prgm2.py
+++
@@ -1,8 +0,0 @@
-def celsius_to_fahrenheit():
-
-  celsius = float(input("Enter temperature in Celsius: "))
-  fahrenheit = (celsius * 9/5) + 32
-  print("Temperature in Fahrenheit:", fahrenheit)
-
-if _name_ == "_main_":
-  celsius_to_fahrenheit()
