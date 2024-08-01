70c9310d637d6697f37f62edc1e8e6e8e1eebe96
Eman Zeb <emanzeb0346@gmail.com>
2024-08-01 16:40:26 +0500

    publish branch

---
+++ Day 5/Day 14/Day 6/prgm9.py
@@ -0,0 +1,9 @@
+def calculate_square():
+
+  number = float(input("Enter a number: "))
+  square = number * number
+
+  print("The square of", number, "is", square)
+
+if _name_ == "_main_":
+  calculate_square()
