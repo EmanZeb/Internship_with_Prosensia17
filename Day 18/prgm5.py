0ee130b755c112c03bdd0b430ec58cfdbe7a39a0
Eman Zeb <emanzeb0346@gmail.com>
2024-08-07 13:14:57 +0500

    publish branch

---
+++ Day 18/prgm5.py
@@ -0,0 +1,7 @@
+def print_keys_and_values(dictionary):
+    keys = list(dictionary.keys())
+    values = list(dictionary.values())
+    print("Keys:", keys)
+    print("Values:", values)
+example_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
+print_keys_and_values(example_dict)
