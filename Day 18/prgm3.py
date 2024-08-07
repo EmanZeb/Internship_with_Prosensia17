149e4a4db2c9706b4dbc1407e3c2df2028f30a2b
Eman Zeb <emanzeb0346@gmail.com>
2024-08-07 13:06:01 +0500

    publish branch

---
+++ Day 18/prgm3.py
@@ -0,0 +1,4 @@
+my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
+print("Dictionary before update:", my_dict)
+my_dict['key2'] = 'new_value2'
+print("Dictionary after update:", my_dict)
